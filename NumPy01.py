27c5c26dd9c7832b0ea46cb4595140e1462d5120
Szollos Marton <marci.szollos@gmail.com>
2024-01-30 19:15:28 +0100

    Matrixes

--- NumPy01.py
+++ NumPy01.py
@@ -15,3 +15,19 @@ result = c[1,5] # works just like in c#
 
 print(result)
 print(c[0, :]) #gives back the whole first row
+
+print(c[0,1:-1:2]) #gives back the first row till the last by 2 steps
+
+#Matrixes
+
+zero = np.zeros((2,3))
+print(zero)
+ones = np.ones((4,3))
+print(ones)
+full = np.full((2,2),15)
+print(full)
+
+#Random
+print(np.random.rand(4,2)) #Random numbers between 0 and 1 4 rows 2 columns
+print(np.random.randint(-3,7,size=(3,3))) # random number between -3 and 7, in the size of an 3x3 matrix
+print(np.identity(3)) #identity matrix one parameter because its a square
