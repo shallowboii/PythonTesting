20eed4589d59c19b837c3065b7e5eaadce42f332
Szollos Marton <marci.szollos@gmail.com>
2024-01-31 12:34:27 +0100

    LinearAlgebra

--- NumPy01.py
+++ NumPy01.py
@@ -31,3 +31,75 @@ print(full)
 print(np.random.rand(4,2)) #Random numbers between 0 and 1 4 rows 2 columns
 print(np.random.randint(-3,7,size=(3,3))) # random number between -3 and 7, in the size of an 3x3 matrix
 print(np.identity(3)) #identity matrix one parameter because its a square
+
+array = np.array([[1,2,3],[1,2,3]])
+r1 = np.repeat(array,3,axis=1) #Repeats an array 
+print(r1)
+print()
+print("Task")
+print("------------------------")
+print()
+output = np.ones((5,5))
+output[1:-1, 1:-1] = 0
+output[2,2] = 9
+print(output)
+
+print()
+print("------------------------")
+print()
+
+#Be careful of copying arrays
+# for example
+array1 = np.array([1,2,3])
+array2 = array1.copy() #if you  want to copy it use the copy function
+array2[0] = 100 # now the array1 and the b refferences the same array in the memory
+print(array1)
+print(array2)
+array1=array1+1 # you can do basic arithmetics
+print(array1)
+print(np.cos(array1))
+print(array1)
+
+
+print()
+print("------------------------")
+print()
+
+#Linear Algebra
+
+
+lineararray1 = np.ones((2,3))
+print(lineararray1)
+
+lineararray2 = np.full((3,2),2) #matrix multiplication
+print(lineararray2)
+
+lineararray_result = np.matmul(lineararray1,lineararray2)
+print(lineararray_result)
+
+            #Determinant
+lineararray3 = np.identity(3)
+print(np.linalg.det(lineararray3))
+
+#Statistics
+
+stat  = np.array([[1,2,3],[4,5,6]])
+print(np.min(stat,axis=1)) #gives back the min of the first and second row
+#min, max, sum
+
+#Reorganizing Arrays
+before = np.array([[1,2,3,4],[5,6,7,8]]) #its a 2 by 4 lets make this into a 4 by 2 or a 1 by 8
+
+after = np.reshape(before,(4,2))
+#or
+after = before.reshape((8,1))
+print(after)
+
+#Vertically stacking vectors
+v1 = np.array([1,2,3,4])
+v2 = np.array([5,6,7,8])
+
+v3 = np.vstack([v1,v2,v2])
+print(v3)
+#the same for horizontal stacks
+
